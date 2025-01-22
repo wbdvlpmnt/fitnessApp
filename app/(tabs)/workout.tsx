1a818b3cf92d0a95244b59e4e3588993d31154b9
wbdvlpmnt <jai.gokhale5219@gmail.com>
2025-01-21 20:34:12 -0600

    Creating a new exercise form

--- app/(tabs)/workout.tsx
+++ app/(tabs)/workout.tsx
@@ -1,11 +1,79 @@
-import React from "react";
-import { View, StyleSheet, Text } from "react-native";
+import Button from "@/components/button";
+import React, { useState } from "react";
+import { View, StyleSheet, Text, TextInput, Alert } from "react-native";
 
 export default function Workout() {
+  const [workoutName, setWorkoutName] = useState("");
+  const [exerciseName, setExerciseName] = useState("");
+  const [exerciseDescription, setExerciseDescription] = useState("");
+  const [exerciseDuration, setExerciseDuration] = useState(0);
+  const [exerciseDurationUnits, setExerciseDurationUnits] = useState("");
+
+  function clearForm() {
+    setExerciseName("");
+    setExerciseDescription("");
+    setExerciseDuration(0);
+    setExerciseDurationUnits("");
+  }
+
+  function handleAddExercise() {
+    const newExercisePayload = {
+      exerciseName,
+      exerciseDescription,
+      exerciseDuration,
+      exerciseDurationUnits,
+    };
+    clearForm();
+    Alert.alert("Exercise Added!");
+  }
+
   return (
     <>
       <View style={styles.container}>
-        <Text>Fitness Tracker workout</Text>
+        <View style={styles.inputForm}>
+          <TextInput
+            style={styles.input}
+            placeholder="Workout Name"
+            placeholderTextColor="#aaa"
+            value={workoutName}
+            onChangeText={setWorkoutName}
+          />
+        </View>
+        <View style={styles.inputForm}>
+          <TextInput
+            style={styles.input}
+            placeholder="Exercise Name"
+            placeholderTextColor="#aaa"
+            value={exerciseName}
+            onChangeText={setExerciseName}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Exercise Description"
+            placeholderTextColor="#aaa"
+            value={exerciseDescription}
+            onChangeText={setExerciseDescription}
+          />
+          <TextInput
+            style={styles.input}
+            keyboardType="numeric"
+            placeholderTextColor="#aaa"
+            value={String(exerciseDuration)}
+            onChangeText={(d) => setExerciseDuration(+d)}
+          />
+          <TextInput
+            style={styles.input}
+            placeholder="Repitition Units"
+            placeholderTextColor="#aaa"
+            value={exerciseDurationUnits}
+            onChangeText={setExerciseDurationUnits}
+          />
+          <Button
+            label="Add Exercise"
+            theme="primary"
+            onPress={handleAddExercise}
+          />
+        </View>
       </View>
     </>
   );
@@ -15,6 +83,20 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#25292e",
+    alignItems: "center",
+  },
+  input: {
+    width: "100%",
+    backgroundColor: "#3a3f47",
+    color: "#fff",
+    borderRadius: 8,
+    padding: 10,
+    marginBottom: 15,
+    fontSize: 16,
+  },
+  inputForm: {
+    padding: 20,
+    width: "100%",
     alignItems: "center",
   },
 });
