d2dd596d2dcf4788478c387efee8f437d8f376ea
wbdvlpmnt <jai.gokhale5219@gmail.com>
2025-01-26 18:59:15 -0600

    add base style for start screen

--- app/(tabs)/start.tsx
+++ app/(tabs)/start.tsx
@@ -1,5 +1,27 @@
-import { Text } from "react-native";
+import { Text, StyleSheet, View } from "react-native";
 
 export default function Start() {
-  return <Text>Start Workout</Text>;
+  return (
+    <View style={styles.container}>
+      <Text>Start</Text>
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#25292e",
+    alignItems: "center",
+    paddingTop: 5, // Added padding for better UI
+  },
+  content: {
+    padding: 15,
+    width: "100%",
+    alignItems: "center",
+    display: "flex",
+    justifyContent: "space-between",
+    backgroundColor: "#1e1e1e", // Changed background color for better contrast
+    borderRadius: 10, // Added border radius for better UI
+  },
+});
